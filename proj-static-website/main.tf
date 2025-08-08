2f87e7d488d579ad5a2de47bb5b85102aab966c3
Kaushik <kaushikkajjam23@gmail.com>
2025-08-08 12:23:15 +0530

    s3 static website host complete

--- proj-static-website/main.tf
+++ proj-static-website/main.tf
@@ -15,12 +15,6 @@ provider "aws" {
   region = var.region
 }
 
-variable "region" {
-  description = "The AWS region to deploy resources into"
-  type        = string
-  default     = "us-west-2"
-}
-
 resource "random_id" "rand_id" {
   byte_length = 8
 }
@@ -29,13 +23,37 @@ resource "aws_s3_bucket" "mywebapp-bucket" {
   bucket = "demobucket-${random_id.rand_id.hex}"
 }
 
-resource "aws_s3_account_public_access_block" "mywebapp-bucket" {
+# Enable ACLs for the bucket
+resource "aws_s3_bucket_ownership_controls" "mywebapp-bucket" {
   bucket = aws_s3_bucket.mywebapp-bucket.id
 
-  block_public_acls   = false
-  block_public_policy = false
-  ignore_public_acls  = false
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "mywebapp-bucket" {
+  bucket = aws_s3_bucket.mywebapp-bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
   restrict_public_buckets = false
+
+  depends_on = [aws_s3_bucket_ownership_controls.mywebapp-bucket]
+}
+
+# Website configuration
+resource "aws_s3_bucket_website_configuration" "mywebapp" {
+  bucket = aws_s3_bucket.mywebapp-bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
 }
 
 resource "aws_s3_object" "index_html" {
@@ -43,13 +61,19 @@ resource "aws_s3_object" "index_html" {
   bucket      = aws_s3_bucket.mywebapp-bucket.bucket
   key         = "index.html"
   acl         = "public-read"
+  content_type = "text/html"
+  
+  depends_on = [aws_s3_bucket_public_access_block.mywebapp-bucket]
 }
 
-resource "aws_s3_object" "style_css" {
-  source      = "./style.css"
+resource "aws_s3_object" "styles_css" {
+  source      = "./styles.css"
   bucket      = aws_s3_bucket.mywebapp-bucket.bucket
-  key         = "style.css"
+  key         = "styles.css"
   acl         = "public-read"
+  content_type = "text/html"
+  
+  depends_on = [aws_s3_bucket_public_access_block.mywebapp-bucket]
 }
 
 output "bucket_name" {
@@ -62,4 +86,34 @@ output "bucket_id" {
 
 output "random_id" {
   value = random_id.rand_id.hex
+}
+
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.mywebapp.website_endpoint
+}
+
+resource "aws_s3_bucket_policy" "mywebapp_policy" {
+  bucket = aws_s3_bucket.mywebapp-bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.mywebapp-bucket.arn}/*"
+      },
+      {
+        Sid       = "PublicPutObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:PutObject"
+        Resource  = "${aws_s3_bucket.mywebapp-bucket.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.mywebapp-bucket]
 }
